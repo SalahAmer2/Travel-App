5020d462870731133effb91b8a84f0fcd994ae31
SalahAmer2 <salahayman2@gmail.com>
2020-05-31 06:03:26 +0200

    Add gitignore, Procfile and update ports

--- src/server/server.js
+++ src/server/server.js
@@ -115,8 +115,16 @@ app.post('/deleteTrip', (req, res) => {
 });
 
 // Setup Server
-const port = '3000';
+// const port = '3000';
 
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
